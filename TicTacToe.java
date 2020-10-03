0586789776844bc3c2172bcb3b1051b754df7ebf
“AHEMMED-1525” <ahemmedhussain1525@gmail.com>
2020-10-03 13:43:54 +0530

    uc3 showing the board

--- TicTacToe.java
+++ TicTacToe.java
@@ -4,7 +4,7 @@ public class TicTacToe
 {
 	char[] gameBoard = new char[10];
 	int index;
-	
+	String playerValue;
 	Scanner input = new Scanner(System.in);
 		/* User Case 1*/
 		public void initializBoard() // Initializing the board
@@ -40,13 +40,18 @@ public class TicTacToe
 		public void showBoard()
 		{
 			System.out.println(" The board is \n");
-			System.out.println(gameBoard[1]+ " | " + gameBoard[2] + " | "+ gameBoard[3] +"\n" + gameBoard[4] + " | " + gameBoard[5] + " | " + gameBoard[6] +"\n" + gameBoard[7] + " | " + gameBoard[8] + " | " + gameBoard[9]);	
+			System.out.println(gameBoard[1]+ " | " + gameBoard[2] + " | "+ gameBoard[3] +"\n"
+								+ gameBoard[4] + " | " + gameBoard[5] + " | " + gameBoard[6] +"\n" 
+								+ gameBoard[7] + " | " + gameBoard[8] + " | " + gameBoard[9]);	
+			
 		}
+
 		public static void main(String args[])
 		{
 			TicTacToe game = new  TicTacToe();
 			game.initializBoard();
 			game.readPlayer();
 			game.showBoard();
+			
 		}
 }
