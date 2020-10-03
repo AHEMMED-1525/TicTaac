95444a234e2461c699d5882e3de3b40f2049c01f
“AHEMMED-1525” <ahemmedhussain1525@gmail.com>
2020-10-03 13:38:10 +0530

    uc1 initializing board

--- TicTacToe.java
+++ TicTacToe.java
@@ -14,39 +14,11 @@ public class TicTacToe
 				gameBoard[index] = '-';
 			}
 		}
-		/* UserCase 2 */
-		public void readPlayer()
-		{
-			String compChar;
-			System.out.println("Enter a character X or O ");
-			String playerChoice = input.nextLine();
-			if (playerChoice.equals("X"))
-			{
-				System.out.println("User has Entered X");
-				compChar = "O";
-				System.out.println(" computer has " +compChar);
-				
-			}
-			else
-			{
-				System.out.println("User has Entered O");
-				compChar = "X";
-				System.out.println("computer has " + compChar);
-			}
-			input.close();
-			
-		}
-		/* User case 3*/
-		public void showBoard()
-		{
-			System.out.println(" The board is \n");
-			System.out.println(gameBoard[1]+ " | " + gameBoard[2] + " | "+ gameBoard[3] +"\n" + gameBoard[4] + " | " + gameBoard[5] + " | " + gameBoard[6] +"\n" + gameBoard[7] + " | " + gameBoard[8] + " | " + gameBoard[9]);	
-		}
+
 		public static void main(String args[])
 		{
 			TicTacToe game = new  TicTacToe();
 			game.initializBoard();
-			game.readPlayer();
-			game.showBoard();
+
 		}
 }
